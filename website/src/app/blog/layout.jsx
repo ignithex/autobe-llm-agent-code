ae10614e1b2cc024cd14aa7e6243fd9671e6c142
Jeongho Nam <samchon.github@gmail.com>
2026-03-28 01:52:20 +0900

    fix blog layout

--- website/src/app/blog/layout.jsx
+++ website/src/app/blog/layout.jsx
@@ -1,20 +1,11 @@
 import "nextra-theme-blog/style.css";
 import "./blog.css";
-import { getPageMap } from "nextra/page-map";
-import { Footer, Layout, Navbar, ThemeSwitch } from "nextra-theme-blog";
+import { Footer, Layout } from "nextra-theme-blog";
 
 export const metadata = {
   title: "Blog",
   description: "Engineering notes, releases, and deep dives from AutoBE.",
 };
-
-function NavLink(props) {
-  return (
-    <a href={props.href} style={{ fontWeight: 600 }}>
-      {props.children}
-    </a>
-  );
-}
 
 export default async function BlogLayout(props) {
   return (
@@ -23,13 +14,6 @@ export default async function BlogLayout(props) {
         defaultTheme: "dark",
       }}
     >
-      <Navbar pageMap={await getPageMap("/blog")}>
-        <NavLink href="/">Home</NavLink>
-        <NavLink href="/docs/">Docs</NavLink>
-        <NavLink href="/blog/">Blog</NavLink>
-        <NavLink href="/blog/rss.xml">RSS</NavLink>
-        <ThemeSwitch />
-      </Navbar>
       {props.children}
       <Footer>
         Released under the AGPL-3.0 License. Copyright 2024 - {new Date().getFullYear()}{" "}
