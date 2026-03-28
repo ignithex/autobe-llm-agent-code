10c97a53c752476b870610358711ee5d996dfa94
Jeongho Nam <samchon.github@gmail.com>
2026-03-28 11:28:00 +0900

    fix layout problem

--- website/src/app/blog/layout.jsx
+++ website/src/app/blog/layout.jsx
@@ -9,13 +9,14 @@ export const metadata = {
 
 export default async function BlogLayout(props) {
   return (
-    <Layout
-      nextThemes={{
-        defaultTheme: "dark",
-      }}
-    >
-      {props.children}
-      <Footer>
+    <div className="blog-page">
+      <Layout
+        nextThemes={{
+          defaultTheme: "dark",
+        }}
+      >
+        {props.children}
+        <Footer>
         Released under the AGPL-3.0 License. Copyright 2024 - {new Date().getFullYear()}{" "}
         <a href="https://github.com/wrtnlabs" target="_blank" style={{ color: "inherit" }}>
           Wrtn Technologies
@@ -23,5 +24,6 @@ export default async function BlogLayout(props) {
         .
       </Footer>
     </Layout>
+    </div>
   );
 }
